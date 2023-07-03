f2aa6acbb17aafdf3a3ae7e262dcf0e48a79dec0
Argiro <elto.82@gmai.com>
2023-07-03 14:09:27 -0500

    componente productos

---
+++ 06-shopping-cart/src/App.jsx
@@ -0,0 +1,15 @@
+import { products as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/Products";
+import { useState } from "react";
+
+function App() {
+  const [products] = useState(initialProducts);
+  return (
+    <>
+      <h1>Shoppin Cart 🛒</h1>
+      <Products products={products} />
+    </>
+  );
+}
+
+export default App;
